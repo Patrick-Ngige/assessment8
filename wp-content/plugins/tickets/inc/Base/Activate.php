61c84a8466242d491ae97eed0cd5d6ae4a11aac3
Hopp-Murithi <hoppmurithi@gmail.com>
2023-05-27 16:59:39 +0300

    feat: implement activate and deactivate for plugin

---
+++ wp-content/plugins/tickets/inc/Base/Activate.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package tickets
+ *
+ */
+
+namespace Inc\Base;
+
+class Activate{
+    public static function activate(){
+        flush_rewrite_rules();
+    }
+}
---
+++ wp-content/plugins/tickets/inc/Base/Deactivate.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package tickets
+ *
+ */
+
+namespace Inc\Base;
+
+class Deactivate{
+    public static function deactivate(){
+        flush_rewrite_rules();
+    }
+}
