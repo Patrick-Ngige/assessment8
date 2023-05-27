6ba8b3ecb25770f9d59182dfc6b41127bb057f57
Hopp-Murithi <hoppmurithi@gmail.com>
2023-05-27 16:59:09 +0300

    chore: Add composer

---
+++ wp-content/plugins/tickets/tickets.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @package tickets
+ */
+
+/*
+Plugin Name: Tickets
+Plugin URI: http://...
+Description: A ticket management system for project managers and employees
+Version: 1.0.0
+Author: Hope ,Nicholas,Patrick
+Author URI: http://...
+License: GPLv2 or Later
+Text Domain: Tickets plugin
+*/
+
+//Security Check 
+
+defined('ABSPATH') or die("Caught you hacker");
+
+//Require once the Composer Autoload
+if(file_exists(dirname(__FILE__).'/vendor/autoload.php')){
+    require_once dirname(__FILE__).'/vendor/autoload.php';
+}
+
+use Inc\Base;
+function activate_tickets_plugin(){
+    Base\Activate::activate();
+}
+register_activation_hook(__FILE__, 'activate_tickets_plugin');
+
+function deactivate_tickets_plugin(){
+    Base\Deactivate::deactivate();
+}
+register_deactivation_hook(__FILE__, 'deactivate_tickets_plugin');
