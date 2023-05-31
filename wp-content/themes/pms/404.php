37a31a354cd0a8521d851a1739abdd86880abd10
Patrick-Ngige <wakemanjajr@gmail.com>
2023-05-31 14:37:09 +0300

    final changes:restricting wp-admin and wp-login.php

---
+++ wp-content/themes/pms/404.php
@@ -0,0 +1,6 @@
+<?php get_header() ?>
+
+<div style="width:97vw; height:80vh; background-size:cover; background-repeat:no-repeat; background-image: url('https://images.unsplash.com/photo-1578328819058-b69f3a3b0f6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8NDA0JTIwZXJyb3J8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60'); ">
+
+</div>
+<?php get_footer() ?>
